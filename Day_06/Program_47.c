29eed30d53e43ebb6c8da251677a6893f521b7df
Anushka Koner <anushkakoner@gmail.com>
2026-09-14 11:27:26 +0530

    Create Program_47.c

---
+++ Day_06/Program_47.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,result;
+	printf("enter no ");
+	scanf("%d",&n);
+	int p=n;
+	while(p!=0)
+	{
+		r=p%10;
+		result+=r*r*r;
+		p/=10;
+	}
+		if(result==n)
+		{
+			printf("armstrong");
+		}
+		else
+		{
+			printf("not armstrong");
+		}
+		
+	
+	
+	return 0;
+}
