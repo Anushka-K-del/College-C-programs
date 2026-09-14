c9131acf1097934bd52a76855f9487b7269798ce
Anushka Koner <anushkakoner@gmail.com>
2026-09-14 11:24:11 +0530

    Add Program_44.c to sum digits of a number
    Implement a program to calculate the sum of digits of a number.

---
+++ Day_06/Program_44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,s,r;
+	printf("enter no ");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		r=n%10;
+		s=s+r;
+		n=n/10;
+	
+	}
+	printf("Sum of all digits= %d \n",s);
+	return 0;
+}
